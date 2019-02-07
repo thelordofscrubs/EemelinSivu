56b247ea5c732547ef644eaed4f596c63f4ad62f
thelordofscrubs <ekaituri@gmail.com>
2019-02-07 16:07:34 +0200

    wrote code into MOBA.js and fiddled with MOBA.css

---
+++ EemelinSivu/JS/Navbar.js
@@ -0,0 +1,11 @@
+function NavBar() {
+
+document.getElementById("navbar").innerHTML= "\
+<a href='JSlist.html'>Javascript</a>\
+<a href=''>Pictures</a>\
+<a href='OtherStuff.html'>Misc</a>\
+<div id = 'more'>\
+    <a href='index.html'>Main</a>\
+</div>\
+";
+}
