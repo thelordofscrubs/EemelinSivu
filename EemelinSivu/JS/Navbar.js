4650eb1bf548f65ad149cd44404b980bb450625c
thelordofscrubs <ekaituri@gmail.com>
2019-02-11 21:36:30 +0200

    redid whole folder really

--- EemelinSivu/JS/Navbar.js
+++ EemelinSivu/JS/Navbar.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function NavBar() {
 
 document.getElementById("navbar").innerHTML= "\
@@ -9,16 +8,4 @@ document.getElementById("navbar").innerHTML= "\
     <a href='index.html'>Main</a>\
 </div>\
 ";
-=======
-function NavBar() {
-
-document.getElementById("navbar").innerHTML= "\
-<a href='JSlist.html'>Javascript</a>\
-<a href=''>Pictures</a>\
-<a href='OtherStuff.html'>Misc</a>\
-<div id = 'more'>\
-    <a href='index.html'>Main</a>\
-</div>\
-";
->>>>>>> e934609dbd71d2d8ce2af3e516fec7d988d9335b
 }
