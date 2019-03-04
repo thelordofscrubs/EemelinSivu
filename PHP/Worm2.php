11b50471100f434acda8b60037cdb8ab81a046fb
Kaituri Eemeli <bgh221@myy.haaga-helia.fi>
2019-03-04 13:06:06 +0200

    php hesitantly done

--- PHP/Worm2.php
+++ PHP/Worm2.php
@@ -22,7 +22,11 @@ for ($i = 0; $i < $xmldom->getElementsByTagName("name")->length; $i++ ) {
     $scores[$i]->speed = (int)$xmldom->getElementsByTagName("speed")[$i]->firstChild->nodeValue;
     $scores[$i]->name = $xmldom->getElementsByTagName("name")[$i]->firstChild->nodeValue;
     $scores[$i]->hs = (int)$xmldom->getElementsByTagName("scoreH")[$i]->firstChild->nodeValue;
+    $scores[$i]->id = $xmldom->getElementsByTagName("scorev")[$i]->getAttribute("id");
 }
+
+
+
 
 if ($scores->length < 5) {
     pushScore();
@@ -38,19 +42,50 @@ exit("smaller than smallest score");
 
 
 
+function findID() {
+    $ids = [];
+    for ($i = 0; $i < $scores->length; $i++ ) {
+        $ids[$i] = $scores[$i]->id;
+    }
+    sort($ids);
+    $bid = $ids[0];
+
+    return $bid;
+}
 
 function pushScore() {
     $loc = sortScores();
-    if ($scores->length <) {
-
+    $newscoredom = $xmldom->createElement("scorev");
+    $idat = $xmldom->createAttribute("id");
+    $idat->nodeValue = (findID() + 1);
+    $newscoredom->setAttributeNode($idat);
+    $newel = $xmldom->createElement("score");
+    $newel->appendChild($xmldom->createTextNode($score));
+    $newscoredom->appendChild($newel);
+    $newel = $xmldom->createElement("name");
+    $newel->appendChild($xmldom->createElement($name));
+    $newscoredom->appendChild($newel);
+    $newel = $xmldom->createElement("speed");
+    $newel->appendChild($xmldom->createElement($speed));
+    $newscoredom->appendChild($newel);
+    $newel = $xmldom->createElement("size");
+    $newel->appendChild($xmldom->createElement($size));
+    $newscoredom->appendChild($newel);
+    $newel = $xmldom->createElement("hs");
+    $newel->appendChild($xmldom->createElement($hs));
+    $newscoredom->appendChild($newel);
+    $xmldom->documentElement->insertBefore($newscoredom, $xmldom->getElementsByTagNam("scorev")[$loc]);
+    if ($scores->length = 5) {
+        $xmldom->getElementsByTagName("scorev")[4]->removeChild($xmldom->getElementsByTagName("scorev")[4]);
     }
-    $xmldom->getElementsByTagName("scorev")[]->removeChild
+    $xmldom->formatOutput = true;
+    $xmldom->saveXML();
 }
 
 function sortScores() {
     $spot = 4;
     for($i = 0;$i < $scores->length;$i++) {
-        if ($hs > ) {
+        if ($hs > $scores[$i]->hs) {
             $spot = $i;
             break;
         }
