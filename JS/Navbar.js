c8c90756e15d49d89521d5d7fb0521cb608ddd06
thelordofscrubs <ekaituri@gmail.com>
2019-03-08 23:07:28 +0200

    started guest book

--- JS/Navbar.js
+++ JS/Navbar.js
@@ -4,6 +4,7 @@ document.getElementById("navbar").innerHTML= "\
 <a href='/JSlist.html'>Javascript</a>\
 <a href=''>Pictures</a>\
 <a href='/OtherStuff.html'>Misc</a>\
+<a href='/vk.html'>Guest Book(Vieras Kirja)</a>\
 <div id = 'more'>\
     <a href='/index.html'>Main</a>\
 </div>\
