7ebbbd24580f5c7719e318609bf9d51c0139b1bd
Kaituri Eemeli <bgh221@myy.haaga-helia.fi>
2019-03-12 12:16:30 +0200

    vk

---
+++ JS/vk.js
@@ -0,0 +1,14 @@
+var commentArray = [];
+
+
+
+
+function submitComment() {
+    var commentField = document.getElementById("commentin");
+    var commentContent = commentField.value;
+}
+
+function comment(content, name) {
+    this.content = content;
+    this.name = name;
+}
