36b1610f16e1d10aab981605031d3ed0ff8f2e8e
Kaituri Eemeli <bgh221@myy.haaga-helia.fi>
2019-04-01 11:39:46 +0300

    adding fancy page for kasper

--- JS/Navbar.js
+++ JS/Navbar.js
@@ -5,6 +5,7 @@ document.getElementById("navbar").innerHTML= "\
 <a href=''>Pictures</a>\
 <a href='/OtherStuff.html'>Misc</a>\
 <a href='/vk.html'>Guest Book(Vieras Kirja)</a>\
+<a href='/fancy.html'>CSS project (Kasper)</a>\
 <div id = 'more'>\
     <a href='/index.html'>Main</a>\
 </div>\
