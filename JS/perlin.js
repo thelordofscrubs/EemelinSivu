84b87d4080c94f1ee1ab93f19d93918411541339
thelordofscrubs <ekaituri@gmail.com>
2019-04-18 22:52:39 +0300

    pretty much finished a few versions of noise gen

--- JS/perlin.js
+++ JS/perlin.js
@@ -4,8 +4,8 @@ function createNoise(alength) {
     let noise = [];
     //starting point is half
     noise[0] = .5;
-    for (let i = 0; i < alength; i++) {
-        let chg = Math.random()*.04
+    for (let i = 0; i < alength/2; i++) {
+        let chg = Math.random()*.1
         //if (Math.random() < noise[i]) {
         //    chg *= (-1);
         //}
@@ -13,17 +13,19 @@ function createNoise(alength) {
             chg *= (-1);
         }
         //if the number is high, then higher probability for it to go down, same for low but opposite
-        if (chg < 0 && noise[i] < .15) {
+        if (chg < 0 && noise[i*2] < .2) {
             if (Math.random() > noise[i]) {
                 chg *= (-1);
             }
         }
-        if (chg > 0 && noise[i] > .85) {
+        if (chg > 0 && noise[i*2] > .8) {
             if (Math.random() < noise[i]) {
                 chg *= (-1);
             }
         }
-        noise[i+1] = noise[i] + chg;
+        noise[i*2+2] = noise[i*2] + chg;
+        noise[i*2+1] = noise[i*2]/2 + noise[i*2+2]/2;
+        
     }
     //for(let i = 0; i < noise.length; i++) {
     //    noise[i] -= .5
@@ -38,8 +40,43 @@ function twoDNoise(dimL) {
         yArray[i] = [];
         yArray[i][0] = t[i];
         for (let f = 1; f < dimL; f++) {
-            let OG = (yArray[i][f-1]+yArray[i-1][f])/2;
-            let chg = Math.random()*.01
+            
+            yArray[i][f] = yArray[i][0]/2+yArray[0][f]/2;
+            //yArray[i][f] -= (Math.min(.1,yArray[i][f]))
+        }
+    }
+    return yArray;
+}
+
+function noiseObject(NV, n) {
+    this.noiseValue = NV;
+    this.neighbors = n;
+}
+
+function twoDNoise2(dimL) {
+    //let yArray = createNoise(dimL);
+    let oArray = [createNoise(dimL)];
+    /*
+    for (let i = 0; i<dimL;i++) {
+        let n = 2
+        if (i == 0 || i == dimL-1) {
+            n = 1
+        }
+        oArray[0][i] = yArray[i];//new noiseObject(yArray[i],n);
+    } 
+    */
+    for (let i = 1; i < dimL ; i++) {
+        oArray[i] = [];
+        for (let f = 1; f < dimL; f++) {
+            let pL = oArray[i-1];
+            let OG = (pL[f]+pL[f-1]+pL[f+1])/3;
+            if (f == 1) {
+                OG = (pL[f]+pL[f+1])/2;
+            }
+            if (f == dimL-1) {
+                OG = (pL[f]+pL[f-1])/2;
+            }
+            let chg = Math.random()*.08;
             if (Math.random()<.5) { 
                 chg *= (-1);
                 if (OG < .1 && Math.random() > OG) {
@@ -48,12 +85,24 @@ function twoDNoise(dimL) {
             } else if (OG > .98 && Math.random() < OG) {
                 chg *= (-1);
             }
-            
-            yArray[i][f] = OG + chg;
+            oArray[i][f] = OG + chg;
         }
     }
-    return yArray;
+    return oArray;
 }
+
+/*
+function threeDNoise(dim) {
+    let zArray = [twoDNoise(dim)];
+    let t1 = twoDNoise(dim);
+    let t2 = twoDNoise(dim);
+    for (let i = 1; i < dim; i++) {
+        zArray[i] = [];
+
+        
+    }
+}
+*/
 
 function displayTwoDNoise() {
     if (!document.getElementById("canvasElement")) {
@@ -68,11 +117,15 @@ function displayTwoDNoise() {
     for (let i = 0; i < 500; i++) {
         for (let f = 0; f < 500; f++) {
             let colorString = "#";
-            colorString1 = colorString.concat(Math.round(noiseToDisplay[i][f]*100).toString(16));
-            colorString = colorString1.concat(Math.round(noiseToDisplay[i][f]*100).toString(16));
-            colorString1 = colorString.concat(Math.round(noiseToDisplay[i][f]*100).toString(16));
-            ctx.fillStyle = colorString1;
-            ctx.fillRect(i,f,1,1);            
+            let lv = Math.round(noiseToDisplay[i][f]*100).toString(16)
+            if (lv.length == 1) {
+                lv = "0".concat(lv);
+            }
+            colorString = colorString.concat(lv);
+            colorString = colorString.concat(lv);
+            colorString = colorString.concat(lv);
+            ctx.fillStyle = colorString;
+            ctx.fillRect(f,i,1,1);            
         }
     }
 }
