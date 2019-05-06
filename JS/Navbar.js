1decd551d95b29e6247c62b2ae051877fedc0f51
Kaituri Eemeli <bgh221@myy.haaga-helia.fi>
2019-05-06 10:17:46 +0300

    finishing touches

--- JS/Navbar.js
+++ JS/Navbar.js
@@ -13,8 +13,9 @@ document.getElementById("navbar").innerHTML= "\
 }
 
 function displayNav() {
-    if (document.getElementById("more1")) {
-        document.getElementById("navBut").innerHTML="";
+    if (document.getElementById("navBut1")) {
+        document.getElementById("navBut1").innerHTML="";
+        document.getElementById("navBut1").setAttribute("id","navBut");
     } else {
         document.getElementById("navBut").innerHTML="\
         <a href='/JSlist.html'>Javascript</a>\
@@ -26,5 +27,6 @@ function displayNav() {
             <a href='/index.html'>Main</a>\
         </div>\
         ";
+        document.getElementById("navBut").setAttribute("id","navBut1");
     }
 }
